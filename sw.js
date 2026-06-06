fcd13ed0092f1d482c3b3486f14911bb8a396bdc
LiuKunyi <96640695+LiuKunyi@users.noreply.github.com>
2026-06-07 00:57:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -50,6 +50,22 @@ self.addEventListener("fetch", event => {
 
             return response || fetch(event.request);
 
+    self.addEventListener(
+    "message",
+    event=>{
+
+        if(
+            event.data &&
+            event.data.type==="SKIP_WAITING"
+        ){
+
+            self.skipWaiting();
+
+        }
+
+    }
+);
+
         })
 
     );
